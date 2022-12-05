11b125cc7084114c220a6060e67c05df0c9a7108
Tarandeep Singh <55691535+singhstack@users.noreply.github.com>
2022-12-05 12:41:16 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+'''Code Flow
+Run Through the maze
+    1. request user for inputs to build maze - which method to use
+        get length and width and return maze
+    2. Show the maze
+    3. User satisifed? Move on with Solving maze
+    4. Choose Method to solve the maze - solve maze using that method
+        BFS -
+        DFS -
+        Brute force -
+        Djikstra's Algorithm -
+        Have other methods in mind? Add yours and build code for it.
+    5. Show User the time taken,
+    6. Would you like to find out how long other methods take to solve this maze?
+    7. a) No? Game Ends with a summary
+    7. b) Yes? Solve Maze using other methods and show the time taken.
+'''
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Taran')
+    print('Debuuger running')
+    print("Code ended")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
